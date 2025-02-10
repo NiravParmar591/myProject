9293e25ede11e0ffdeda13f14d9682437a527465
NiravParmar591 <niravparmar591@gmail.com>
2025-02-10 21:30:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+let btn = document.querySelectorAll(".btn");
+
+Array.from(btn).forEach((e) => {
+    let num = document.querySelector(".num");
+    // e.addEventListener("click" , () => {
+        //     console.log("btn is clicked");
+        //     console.log(e.value);
+        //     num.innerHTML = e.value; 
+        
+        //     e.classList.add("active");
+        // });
+        
+    e.addEventListener("click" , () => {
+        // console.log("button is clicked");           
+        Array.from(btn).forEach((b) => {
+            b.classList.remove("active");
+        });
+        e.classList.add("active");
+        num.innerHTML = e.value;
+    });
+
+});
+
+document.querySelector(".sub_btn").addEventListener("click" , () => {    
+    document.querySelector(".con").style.display = "none";
+    document.querySelector(".message").style.display = "block";
+}); 
